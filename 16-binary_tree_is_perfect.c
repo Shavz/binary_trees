41f6803089663c250fd2ff397e4911b31f9e43d6
Shavz <104979137+Shavz@users.noreply.github.com>
2022-11-16 21:39:15 +0200

    16. Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,9 +3,9 @@
 /**
  * binary_tree_is_perfect - checks if binary tree is perfect
  * @tree: pointer to root of tree
- *
  * Return: 1 if tree is perfect, 0 otherwise
  */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t num_nodes, i;
@@ -20,9 +20,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 /**
  * binary_tree_size - calculates the size of the tree
  * @tree: pointer to root of tree
- *
  * Return: size of tree or 0 if NULL
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
@@ -33,9 +33,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 /**
  * binary_tree_height - measures height of tree
  * @tree: pointer to root of tree
- *
  * Return: height of tree, 0 if tree is NULL
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lsum;
