151d504a5dc740be18e8228ced8277457b5be6db
Shavz <104979137+Shavz@users.noreply.github.com>
2022-11-16 22:39:04 +0200

    36. Is Binary heap

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -3,9 +3,9 @@
 /**
  * binary_tree_is_heap - checks if binary tree is a max binary heap
  * @tree: pointer to root node of tree
- *
- * Return: 1 if valid max heap, 0 otherwise
+ * Return: 1 if valid max heap or 0 otherwise
  */
+
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
 	if (tree == NULL)
@@ -15,14 +15,14 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 }
 
 /**
- * heap_check - recursively checks if tree is a max heap
+ * heap_check - checks if tree is a max heap
  * @tree: pointer to root of tree
  * @max: maximum value node can have to be valid
  * @size: number of nodes in tree
  * @index: current index of node
- *
- * Return: 1 if valid max heap, 0 otherwise
+ * Return: 1 if valid max heap or 0 if not
  */
+
 int heap_check(const binary_tree_t *tree, int max, size_t size, size_t index)
 {
 	if (tree == NULL)
@@ -36,9 +36,9 @@ int heap_check(const binary_tree_t *tree, int max, size_t size, size_t index)
 /**
  * binary_tree_size - calculates the size of the tree
  * @tree: pointer to root of tree
- *
  * Return: size of tree or 0 if NULL
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
