72e9ca9bb162dd76d4b5bd2352f97162a4584012
Shavz <104979137+Shavz@users.noreply.github.com>
2022-11-16 21:33:51 +0200

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures height of tree
+ * binary_tree_height - measures height of a binary tree
  * @tree: pointer to root of tree
- *
- * Return: height of tree, 0 if tree is NULL
+ * Return: height of tree or 0 if tree is NULL
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lsum;
