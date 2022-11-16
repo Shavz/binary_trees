60624d629ed493f44e16483fd33e615620d02b34
Shavz <104979137+Shavz@users.noreply.github.com>
2022-11-16 21:26:33 +0200

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,9 +4,9 @@
  * binary_tree_node - creates a binary tree node
  * @parent: pointer to parent
  * @value: value of node
- *
- * Return: pointer to new node or NULL if fail
+ * Return: pointer to new node or NULL if it fails
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
