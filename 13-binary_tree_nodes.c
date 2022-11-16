2763f2ac42351a2471548a752dbab62cda546bbf
Shavz <104979137+Shavz@users.noreply.github.com>
2022-11-16 21:36:25 +0200

    13. Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,9 +3,9 @@
 /**
  * binary_tree_nodes - count number of nodes with at least 1 child
  * @tree: pointer to root of tree
- *
  * Return: number of nodes with at least on child, or 0 if NULL
  */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
