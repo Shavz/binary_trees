7d8b33425321d02d9a09b76e8051b0d2b1416cac
Shavz <104979137+Shavz@users.noreply.github.com>
2022-11-16 22:15:15 +0200

    26. BST - Array to BST

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst - converts an array to a BST
+ * array_to_bst - converts an array to BST
  * @array: array to convert
  * @size: size of array
- *
- * Return: pointer to root of BST, or NULL if fail
+ * Return: pointer to root of BST or NULL if fail
  */
+
 bst_t *array_to_bst(int *array, size_t size)
 {
 	size_t i;
